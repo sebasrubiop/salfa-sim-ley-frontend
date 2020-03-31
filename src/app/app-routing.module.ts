4d0e0b1e3977d5d2bb66eb4461f5741077bbd067
Sebastian Rubio <sebarubiop@gmail.com>
2020-03-31 10:30:15 -0300

    routing primeros

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,37 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { InstruccionesComponent } from './pages/instrucciones/instrucciones.component';
+import { PersonajesComponent } from './pages/personajes/personajes.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'instrucciones',
+    pathMatch: 'full',
+  },
+  {
+    path: "instrucciones",
+    component: InstruccionesComponent
+  },
+  {
+    path: "personajes",
+    component: PersonajesComponent
+  },
+  {
+    path: "**",
+    redirectTo: "instrucciones",
+    pathMatch: "full"
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+
+export const routedComponents = [
+  InstruccionesComponent,
+  PersonajesComponent
+]
---
+++ src/app/pages/instrucciones/instrucciones.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core'
+
+@Component({
+  selector: 'app-instrucciones',
+  templateUrl: 'instrucciones.component.html',
+  styleUrls: ['instrucciones.component.scss'],
+})
+export class InstruccionesComponent implements OnInit {
+
+  constructor(
+  ) {
+  }
+
+  ngOnInit() {
+  }
+}
