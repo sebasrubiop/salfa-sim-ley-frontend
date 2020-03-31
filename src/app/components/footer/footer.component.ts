dc5147eae7b9e25ebec9dfcbcd2bfaf07a4b6658
Sebastian Rubio <sebarubiop@gmail.com>
2020-03-31 15:40:03 -0300

    mejorando instrucciones

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { Router } from '@angular/router';
 
 @Component({
@@ -8,6 +8,7 @@ import { Router } from '@angular/router';
 })
 export class FooterComponent implements OnInit {
 
+    @Output() action: EventEmitter<any> = new EventEmitter()
     url: string
     continuar: boolean
     comenzar: boolean
@@ -38,8 +39,8 @@ export class FooterComponent implements OnInit {
         }
         if(this.url === '/personajes') {
             this.router.navigate(['/instrucciones'])
-        } else {
-            console.log(this.url)
+        } if(this.url === '/instrucciones') {
+            this.action.emit('next')
         }
     }
 }
