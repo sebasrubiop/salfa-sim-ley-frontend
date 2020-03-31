6c28d7c8ea3bbb9360217d263917c81f660c877a
Sebastian Rubio <sebarubiop@gmail.com>
2020-03-31 13:24:56 -0300

    footer y nuevos ui

---
+++ src/app/components/components.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+
+import { FooterComponent } from './footer/footer.component';
+
+const components =[
+    FooterComponent
+]
+
+@NgModule({
+  declarations: [
+    components
+  ],
+  imports: [
+    CommonModule,
+    RouterModule,
+  ],
+  exports:[
+    components
+  ]
+})
+export class ComponentsModule { }
---
+++ src/app/components/footer/footer.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+    selector: 'app-footer',
+    templateUrl: './footer.component.html',
+    styleUrls: ['./footer.component.scss']
+})
+export class FooterComponent implements OnInit {
+
+    url: string
+    continuar: boolean
+    comenzar: boolean
+
+    constructor(
+        private router: Router,
+    ) {
+    }
+
+    ngOnInit() {
+        this.url = this.router.url
+    }
+
+    onHover(btn: string) {
+        if(btn === 'continuar'){
+            this.continuar = !this.continuar
+            this.comenzar = false
+        } 
+        if(btn === 'comenzar'){
+            this.comenzar = !this.comenzar
+            this.continuar = false
+        } 
+    }
+}
